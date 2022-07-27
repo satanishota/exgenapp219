c964427c5c467ce80bb168af3e2a45e3a842fb5e
satanishota <figuree5252@gmail.com>
2022-07-27 18:36:14 +0900

    first commit

---
+++ routes/hello.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/',(req, res, next) => {
+  var msg = '何か書いて';
+  if (req.session.message !=undefined){
+    msg = "last Message" + req.session.message;
+  }
+  var data = {
+    title:'hello!!',
+    content: msg
+  };
+  res.render('hello', data);
+});
+
+router.post('/post',(req,res,next) => {
+  var msg =req.body['message'];
+  req.session.message = msg;
+  var data = {
+    title: 'hello',
+    content: 'あなたは'+ msg + '送りました' + req.session.message
+  };
+  res.render('hello',data);
+  
+})
+module.exports = router;
